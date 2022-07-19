83ba7c5c414cf159fd6a17a26f176313b3765a91
GarrettSwit <109528396+GarrettSwit@users.noreply.github.com>
2022-07-19 12:58:31 -0700

    Super basic input handling

--- Source.cpp
+++ Source.cpp
@@ -2,26 +2,58 @@
 #include <iomanip>
 #include <string>
 using namespace std;
+//got bored so I did basic input handling, two different kinds one sepeareted into a diff frucntion and the other using cin.fail()
 
-void CubeThis(){
-	float retvalue, x;
-	cout << "Now may I have a value that I may cube?\n";
+
+
+int isInputValid(string str) {
+	int retString;
+	if (isdigit(str[0])) {
+		retString = atoi(str.c_str());
+		return retString;
+	}
+	else {
+		cout << "Invalid Argument ";
+		return 0;
+	}
+	;
+}
+void CubeThis() {
+	int retvalue;
+	string x;
+	//string inputValue;
+	cout << "Now may I have a integer that I may cube?\n";
 	cin >> x;
-	cout <<"Your cubed value of " + std::to_string(x) + " is " + std::to_string(retvalue = pow(x, 3));
+	//input validation
+	if (isInputValid(x)) {
+		cout << "Your cubed value of " + x + " is " + std::to_string(retvalue = pow(atoi(x.c_str()), 3));
+	}
+	else {
+		cout << "Error: Input is not an integer!" << endl;
+		cin.ignore(10000, '\n');;
+		//CubeThis();
+	}
 }
 void SumAverage() {
-	float a, b, c, x;
+	int a, b, c;
 	// Speak to User and request input
 	cout << "Greeting Bug Person,  I request you input three number for math.\n";
 	cout << "You can enter them seperated by spaces, or individual lines.\n";
 	//handle input either in one line or in three sepearate requests
 	cin >> a >> b >> c;
-	cout << "Sum = " + std::to_string((a + b + c)) + "\n";
-	cout << "Average = " + std::to_string((a + b + c) / 3) + "\n";
+	if (cin.fail()) {
+		cout << "Error: Input is not an integer!" << endl;
+		cin.clear();
+	}
+	else {
+		cout << "Sum = " + std::to_string((a + b + c)) + "\n";
+		cout << "Average = " + std::to_string((a + b + c) / 3) + "\n";
+	}
 }
+
 int main() {
 	
-	SumAverage();
+	//SumAverage();
 	CubeThis();
 	return 0;
 }
